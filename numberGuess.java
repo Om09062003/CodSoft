d5b2f099198c83a44c1437c5f4b71a51a1d94ee7
Om09062003 <svpt12omugale65@gmail.com>
2023-10-21 15:30:29 +0530

    add paragraph

--- numberGuess.java
+++ numberGuess.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
-public class Main {
+public class numberGuess {
     public static void main(String[] args) {
 
-        Scanner sc= new Scanner(System.in);
-        int myAnswer = (int)(Math.random()*100);
+        Scanner sc = new Scanner(System.in);
+        int myAnswer = (int) (Math.random() * 100);
         int userNumber = 0;
         int count = 0;
         int attempts = 0;
@@ -12,26 +12,24 @@ public class Main {
             System.out.print("Guess the number (1-100): ");
             userNumber = sc.nextInt();
 
-            if(userNumber == myAnswer){
+            if (userNumber == myAnswer) {
                 System.out.println("OHH YESS... Your Number Is Correct! YOU WON!");
                 break;
-            }
-            else if(userNumber > myAnswer){
+            } else if (userNumber > myAnswer) {
                 System.out.println("Your number TOO HIGH try again");
-            }
-            else {
+            } else {
                 System.out.println("Your number is TOO LOW try again");
             }
 
             count++;
             attempts = count + 1;
 
-            if(count == 10 ){
+            if (count == 10) {
                 System.out.println("Attempts are over");
                 break;
             }
 
-        }while(userNumber >= 0);
+        } while (userNumber >= 0);
 
         System.out.println("Number of times you attempt = " + attempts);
         System.out.println("The Number is: " + myAnswer);
