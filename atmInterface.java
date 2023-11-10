38d27123526bb1acb990b3d73726da0c2ba016a8
Om09062003 <svpt12omugale65@gmail.com>
2023-11-10 12:39:18 +0530

    add paragraph

--- atmInterface.java
+++ atmInterface.java
@@ -1,25 +1,64 @@
 import java.util.Scanner;
 
 public class atmInterface {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] arg) {
 
-        System.out.print("Enter a string: ");
-        String s = scanner.next();
+        double balance = 0;
+        double withdraw = 0;
+        double deposite = 0;
+        Scanner sc = new Scanner(System.in);
 
-        String evenChars = "";
-        String oddChars = "";
+        while (true) {
+            System.out.println("ATM Menu");
+            System.out.println("1.Deposite Money");
+            System.out.println("2.Withdraw Money");
+            System.out.println("3.Check Balance");
+            System.out.println("4.Exit");
 
-        for (int i = 0; i < s.length(); i++) {
-            if (i % 2 == 0) {
-                evenChars += s.charAt(i);
-            } else {
-                oddChars += s.charAt(i);
+            System.out.print("Enter Your Choice: ");
+            int option = sc.nextInt();
+
+            System.out.println("");
+
+            switch (option) {
+                case 1:
+                    System.out.print("Enter The Deposite Amount: ");
+                    deposite = sc.nextDouble();
+                    if (deposite > 0) {
+                        balance = balance + deposite;
+                        System.out.println("Deposite Successful");
+                        System.out.println("");
+                    } else {
+                        System.out.println("Enter Valid Amount");
+                    }
+                    break;
+
+                case 2:
+                    System.out.print("Enter The Withdraw Amount: ");
+                    withdraw = sc.nextDouble();
+                    if (withdraw > 0 && withdraw <= balance) {
+                        balance = balance - withdraw;
+                        System.out.println("Withdraw Successful");
+                        System.out.println("");
+                    } else {
+                        System.out.println("Enter Valid Amount");
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Your Current Balance: " + balance);
+                    System.out.println("");
+                    break;
+
+                case 4:
+                    System.exit(0);
+                    ;
+                    break;
+
+                default:
+                    System.out.println("Enter Valid Choice");
+
             }
         }
-
-        System.out.println(evenChars + " " + oddChars);
-
-        scanner.close();
     }
 }
