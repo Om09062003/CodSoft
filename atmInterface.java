80605523765c69fb6bef98b141c33fdda321f0a5
Om09062003 <svpt12omugale65@gmail.com>
2023-11-10 11:32:13 +0530

    add paragraph

--- atmInterface.java
+++ atmInterface.java
@@ -1,3 +1,25 @@
-public class atmInterface {
+import java.util.Scanner;
 
+public class atmInterface {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String s = scanner.next();
+
+        String evenChars = "";
+        String oddChars = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            if (i % 2 == 0) {
+                evenChars += s.charAt(i);
+            } else {
+                oddChars += s.charAt(i);
+            }
+        }
+
+        System.out.println(evenChars + " " + oddChars);
+
+        scanner.close();
+    }
 }
